8433626066b63aae3780e964ce817eaa315501d3
mladja995 <mladen.mladenovic95@yahoo.com>
2022-04-05 00:38:05 +0200

    User Profile activity implementation based on friendship

---
+++ app/src/main/java/mosis/elfak/basketscheduling/UserPendingFriendRequests.java
@@ -0,0 +1,14 @@
+package mosis.elfak.basketscheduling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPendingFriendRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_pending_friend_requests);
+    }
+}
